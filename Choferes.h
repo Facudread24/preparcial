1b0505a95f9a67a6cab8733ca6592f5049a632f0
Facudread24 <67750216+Facudread24@users.noreply.github.com>
2020-09-14 18:25:42 -0300

    Delete Choferes.h

--- Choferes.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "tipos.h"
-void menuChoferes();
-void altaChofer();
-bool guardarChofer(choferes chof);
-choferes cargarChofer();
-int buscarDni(char *chof);
-void listarChoferes();
-void mostrarChoferes (choferes chof);
